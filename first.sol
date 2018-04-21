438e202b0a98a7a5a6da77ae7b0fe8ef5a0f99f6
edisonkrasniqi <krasniqiedison6@gmail.com>
2018-04-21 12:52:42 +0200

    Fshirja e function mul()

--- first.sol
+++ first.sol
@@ -2,13 +2,6 @@ pragma solidity ^0.4.21;
 
 contract Kontrat {
     
-  function mul(uint a, uint b) public pure returns (uint c) {
-    if (a == 0) {
-      return 0;
-    }
-    assert(c / a == b);
-    return c;
-  }
 
   function div(uint a, uint b) public pure returns (uint) {
     return a / b;
