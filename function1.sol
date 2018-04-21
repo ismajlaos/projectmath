e7149b88b93d19e4e00af1e95eaf6981326b2401
Etnik A. Gashi <37635686+Etnik-Gashi@users.noreply.github.com>
2018-04-21 12:38:17 +0200

    Funksion me errora

--- function1.sol
+++ function1.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.4.21;
 contract Kontrat {
     
   function mul(uint a, uint b) public pure returns (uint c) {
-    if (a == 0) {
+    if (a == 0) 
       return 0;
     }
-    assert(c / a == b);
+    assert(c / a == b)
     return c;
   }
   }
