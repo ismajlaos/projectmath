8da5392cbf400661c864fe7351ca1e2791de6d6d
edisonkrasniqi <32396208+edisonkrasniqi@users.noreply.github.com>
2018-04-21 13:02:39 +0200

    Function mul() added

--- first.sol
+++ first.sol
@@ -2,6 +2,13 @@ pragma solidity ^0.4.21;
 
 contract Kontrat {
     
+  function mul(uint a, uint b) public pure returns (uint c) {
+    if (a == 0) {
+      return 0;
+    }
+    assert(c / a == b);
+    return b;
+  }
 
   function div(uint a, uint b) public pure returns (uint) {
     return a / b;
